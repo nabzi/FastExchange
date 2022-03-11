6848fbce73d423ba87d163da587331cb7b7707ae
nabzi <zeinab.modir@gmail.com>
2022-03-11 16:50:02 +0330

    Add databinding and show balances

--- app/src/main/java/ir/sample/fastexchange/ExchangeViewModel.kt
+++ app/src/main/java/ir/sample/fastexchange/ExchangeViewModel.kt
@@ -3,23 +3,26 @@ package ir.sample.fastexchange
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.util.*
 import kotlin.collections.ArrayList
 
 data class Balance(var amount: Double, var type: Currency)
 
 class ExchangeViewModel : ViewModel() {
-    var balanceList = MutableLiveData<ArrayList<Balance>>()
-
+    private val _balanceList = MutableStateFlow<ArrayList<Balance>>(arrayListOf())
+    val balanceList: StateFlow<ArrayList<Balance>> = _balanceList
     init {
-        balanceList.postValue(
-            arrayListOf<Balance>(
-                Balance(1000.0, Currency.getInstance("EUR")),
-                Balance(0.0, Currency.getInstance("USD")),
-                Balance(0.0, Currency.getInstance("BGP")),
-            )
+        _balanceList.value = Repository.loadBalanceList()
+    }
+}
+object Repository{
+    fun loadBalanceList() : ArrayList<Balance>{
+        return arrayListOf(
+            Balance(1000.0, Currency.getInstance("EUR")),
+            Balance(0.0, Currency.getInstance("USD")),
+            Balance(0.0, Currency.getInstance("BGP")),
         )
     }
-
-
 }
--- app/src/main/java/ir/sample/fastexchange/MainActivity.kt
+++ app/src/main/java/ir/sample/fastexchange/MainActivity.kt
@@ -3,12 +3,16 @@ package ir.sample.fastexchange
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import ir.sample.fastexchange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val exchangeViewModel: ExchangeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        binding.vmodel = exchangeViewModel
 
         initViews()
     }
